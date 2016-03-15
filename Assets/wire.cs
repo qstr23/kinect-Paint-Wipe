400241dd0e4202b5a2d9b7a99d258f0e0fca8cc7
qstr23 <qstr23@hotmail.com>
2016-03-15 14:58:27 -0400

    stuff

---
+++ Assets/wire.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wire : MonoBehaviour {
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 1));
+	}
+}
